1e0d66c1507b3b872048e0f9b00a9a69216b1993
​Rita Tlemcani <ritabt@stanford.edu>
2020-10-25 15:51:04 -0700

    First working version

--- AndroidStudioProjects_Rita/MyMaps/app/src/main/java/com/rita/mymaps/MapsAdapter.kt
+++ AndroidStudioProjects_Rita/MyMaps/app/src/main/java/com/rita/mymaps/MapsAdapter.kt
@@ -1,13 +1,20 @@
 package com.rita.mymaps
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.rita.mymaps.models.UserMap
 
-class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+private const val TAG = "MapsAdapter"
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>, val onClickListener: OnClickListener) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+
+    interface OnClickListener {
+        fun onItemClick(position: Int)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
@@ -18,6 +25,12 @@ class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerV
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val userMap = userMaps[position]
+        holder.itemView.setOnClickListener {
+            Log.i(TAG, "Tapped on position $position")
+            onClickListener.onItemClick(position)
+        }
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
     }
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
