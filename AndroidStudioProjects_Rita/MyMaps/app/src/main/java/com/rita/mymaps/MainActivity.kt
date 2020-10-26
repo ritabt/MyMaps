d7d82a3706b27813c28eec2f9490ea2aa309377f
​Rita Tlemcani <ritabt@stanford.edu>
2020-10-25 21:17:31 -0700

    Completed Version

--- AndroidStudioProjects_Rita/MyMaps/app/src/main/java/com/rita/mymaps/MainActivity.kt
+++ AndroidStudioProjects_Rita/MyMaps/app/src/main/java/com/rita/mymaps/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rita.mymaps
 
 import android.app.Activity
 import android.content.AbstractThreadedSyncAdapter
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -17,9 +18,11 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.rita.mymaps.models.Place
 import com.rita.mymaps.models.UserMap
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.*
 
 private const val TAG = "MainActivity"
 private const val REQUEST_CODE = 1234
+private const val FILENAME = "UserMapsData"
 const val EXTRA_USER_MAP = "EXTRA_USER_MAP"
 const val EXTRA_MAP_TITLE = "EXTRA_MAP_TITLE"
 class MainActivity : AppCompatActivity() {
@@ -31,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userMaps = generateSampleData().toMutableList()
+        userMaps = deserializeUserMaps(this).toMutableList()
         // Set layout manager on the recycler view
         rvMaps.layoutManager = LinearLayoutManager(this)
         // Set adapter on the recycler view
@@ -42,6 +45,7 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity, DisplayMapActivity::class.java)
                 intent.putExtra(EXTRA_USER_MAP, userMaps[position])
                 startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
             }
         })
 
@@ -84,43 +88,64 @@ class MainActivity : AppCompatActivity() {
             Log.i(TAG, "onActivityResult with new map title ${userMap.title}")
             userMaps.add(userMap)
             mapAdapter.notifyItemInserted(userMaps.size - 1)
+            serializeUserMaps(this, userMaps)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
+    private fun serializeUserMaps(context: Context, userMaps: List<UserMap>){
+        Log.i(TAG, "serializeUserMaps")
+        ObjectOutputStream(FileOutputStream(getDataFile(context))).use { it.writeObject(userMaps) }
+    }
+
+    private fun deserializeUserMaps(context: Context) : List<UserMap>{
+        Log.i(TAG, "deserializeUserMaps")
+        val dataFile = getDataFile(context)
+        if(!dataFile.exists()){
+            Log.i(TAG, "data file does not exists")
+            return emptyList()
+        }
+        ObjectInputStream(FileInputStream(dataFile)).use { return it.readObject() as List<UserMap> }
+    }
+
+    private fun getDataFile(context: Context): File {
+        Log.i(TAG, "Getting file from directory ${context.filesDir}")
+        return File(context.filesDir, FILENAME)
+    }
+
     private fun generateSampleData(): List<UserMap> {
         return listOf(
-            UserMap(
-                "Memories from University",
-                listOf(
-                    Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
-                    Place("Gates CS building", "Many long nights in this basement", 37.430, -122.173),
-                    Place("Pinkberry", "First date with my wife", 37.444, -122.170)
-                )
-            ),
-            UserMap("January vacation planning!",
-                listOf(
-                    Place("Tokyo", "Overnight layover", 35.67, 139.65),
-                    Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
-                    Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
-                )),
-            UserMap("Singapore travel itinerary",
-                listOf(
-                    Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
-                    Place("Jurong Bird Park", "Family-friendly park with many varieties of birds", 1.319, 103.706),
-                    Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
-                    Place("Botanic Gardens", "One of the world's greatest tropical gardens", 1.3138, 103.8159)
-                )
-            ),
-            UserMap("My favorite places in the Midwest",
-                listOf(
-                    Place("Chicago", "Urban center of the midwest, the \"Windy City\"", 41.878, -87.630),
-                    Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
-                    Place("Mackinaw City", "The entrance into the Upper Peninsula", 45.777, -84.727),
-                    Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
-                    Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
-                )
-            ),
+//            UserMap(
+//                "Memories from University",
+//                listOf(
+//                    Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
+//                    Place("Gates CS building", "Many long nights in this basement", 37.430, -122.173),
+//                    Place("Pinkberry", "First date with my wife", 37.444, -122.170)
+//                )
+//            ),
+//            UserMap("January vacation planning!",
+//                listOf(
+//                    Place("Tokyo", "Overnight layover", 35.67, 139.65),
+//                    Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
+//                    Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
+//                )),
+//            UserMap("Singapore travel itinerary",
+//                listOf(
+//                    Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
+//                    Place("Jurong Bird Park", "Family-friendly park with many varieties of birds", 1.319, 103.706),
+//                    Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
+//                    Place("Botanic Gardens", "One of the world's greatest tropical gardens", 1.3138, 103.8159)
+//                )
+//            ),
+//            UserMap("My favorite places in the Midwest",
+//                listOf(
+//                    Place("Chicago", "Urban center of the midwest, the \"Windy City\"", 41.878, -87.630),
+//                    Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
+//                    Place("Mackinaw City", "The entrance into the Upper Peninsula", 45.777, -84.727),
+//                    Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
+//                    Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
+//                )
+//            ),
             UserMap("Restaurants to try",
                 listOf(
                     Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
